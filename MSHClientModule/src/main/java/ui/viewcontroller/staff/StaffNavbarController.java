9554895046fceb816c59cf3a34ba599b276bf15d
Silver Narcissus <827011867@qq.com>
2016-11-27 16:02:58 +0800

    view start

---
+++ MSHClientModule/src/main/java/ui/viewcontroller/staff/StaffNavbarController.java
@@ -0,0 +1,8 @@
+package ui.viewcontroller.staff;
+
+/**
+ * Created by SilverNarcissus on 2016/11/27.
+ */
+public class StaffNavbarController {
+
+}
---
+++ MSHClientModule/src/main/java/ui/viewcontroller/staff/StaffViewController.java
@@ -0,0 +1,7 @@
+package ui.viewcontroller.staff;
+
+/**
+ * Created by SilverNarcissus on 2016/11/27.
+ */
+public class StaffViewController {
+}
