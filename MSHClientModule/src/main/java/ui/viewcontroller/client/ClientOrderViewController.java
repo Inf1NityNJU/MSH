d551269f83c01802879e421af7fb4fdc4b735b9e
Sorumi <Sorumi33@gmail.com>
2016-11-29 00:12:10 +0800

    Add new components! AssessmentView

--- MSHClientModule/src/main/java/ui/viewcontroller/client/ClientOrderViewController.java
+++ MSHClientModule/src/main/java/ui/viewcontroller/client/ClientOrderViewController.java
@@ -85,4 +85,28 @@ public class ClientOrderViewController {
             e.printStackTrace();
         }
     }
+
+    /**
+     * 评分评价
+     */
+    public void showAssessmentEditView(OrderVO order) {
+
+        try {
+            FXMLLoader orderLoader = new FXMLLoader();
+            orderLoader.setLocation(Main.class.getResource("../view/client/ClientAssessmentEditView.fxml"));
+            ScrollPane view = orderLoader.load();
+
+//            ClientOrderDetailViewController clientOrderDetailViewController = orderLoader.getController();
+//            clientOrderDetailViewController.setClientViewController(this);
+//            clientOrderDetailViewController.showOrder(order);
+
+            Node node = rootPane.getCenter();
+            stack.push(node);
+
+            rootPane.setCenter(view);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
