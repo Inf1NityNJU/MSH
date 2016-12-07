6241648b0e03d7da9d56da42b5ec2f443cde3a38
Sorumi <Sorumi33@gmail.com>
2016-12-07 22:02:46 +0800

    Fix bug

--- MSHClientModule/src/test/java/promotion/MinPromotionTest.java
+++ MSHClientModule/src/test/java/promotion/MinPromotionTest.java
@@ -6,6 +6,8 @@ import org.junit.Test;
 import util.DateUtil;
 import util.Place;
 import vo.OrderRoomVO;
+import vo.Promotion_HotelVO;
+import vo.Promotion_WebVO;
 
 import java.util.ArrayList;
 import static org.junit.Assert.assertEquals;
@@ -22,8 +24,8 @@ public class MinPromotionTest {
 
     @Test
     public void testMinWebProm(){
-        double discount = minPromotion.getMinWebProm(new DateUtil(2016,01,11), 2, Place.TangShan);
-        assertEquals(0.50, discount, 0);
+        Promotion_WebVO promotion = minPromotion.getMinWebProm(new DateUtil(2016,01,11), 2, Place.TangShan);
+        assertEquals(0.50, promotion.promotionDiscount, 0);
     }
 
 
@@ -31,7 +33,7 @@ public class MinPromotionTest {
 
     @Test
         public void testMinHotelProm(){
-        double discount = minPromotion.getMinHotelProm("02", new DateUtil(2016,01,11), new DateUtil(2016,01,11), "001", 5);
-        assertEquals(0.40, discount, 0);
+        Promotion_HotelVO promotion = minPromotion.getMinHotelProm("02", new DateUtil(2016,01,11), new DateUtil(2016,01,11), "001", 5);
+        assertEquals(0.40, promotion.promotionDiscount, 0);
     }
 }
