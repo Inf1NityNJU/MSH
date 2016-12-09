c1476a89964d240b26033d0b0ec61aabdf81a661
Vivian <1030518209@qq.com>
2016-12-09 16:42:34 +0800

    HotelPromotion_RoomQuantityAddView

---
+++ MSHClientModule/src/main/java/ui/viewcontroller/staff/HotelPromotion_RoomQuantityAddViewController.java
@@ -0,0 +1,70 @@
+package ui.viewcontroller.staff;
+
+import blservice.promotionblservice.PromotionBLService;
+import component.commontextfield.CommonTextField;
+import component.mydatepicker.MyDatePicker;
+import component.statebutton.StateButton;
+import javafx.fxml.FXML;
+import util.DateUtil;
+import util.PromotionType;
+import vo.PromotionVO;
+import vo.Promotion_RoomQuantityVO;
+
+/**
+ * Created by vivian on 16/12/9.
+ */
+public class HotelPromotion_RoomQuantityAddViewController {
+    private PromotionVO promotionVO;
+    private HotelPromotionViewController hotelPromotionViewController;
+    private PromotionBLService promotionBLService;
+
+    @FXML
+    private CommonTextField nameTextField;
+
+    @FXML
+    private StateButton typeButton;
+
+    @FXML
+    private CommonTextField discountTextField;
+
+    @FXML
+    private MyDatePicker startTime;
+
+    @FXML
+    private MyDatePicker endTime;
+
+    @FXML
+    private CommonTextField roomQuantityTextField;
+
+
+    public void setHotelPromotionViewController(HotelPromotionViewController hotelPromotionViewController) {
+        this.hotelPromotionViewController = hotelPromotionViewController;
+
+        typeButton.setText(PromotionType.Hotel_Birthday.getType());
+        typeButton.setColorProperty(PromotionType.Hotel_Birthday.getColor());
+    }
+
+    public void setPromotionBLService(PromotionBLService promotionBLService){
+        this.promotionBLService = promotionBLService;
+    }
+
+    public void clickCancelButton(){
+        hotelPromotionViewController.refreshWebPromotionList();
+        hotelPromotionViewController.back();
+    }
+
+    public void clickSaveButton(){
+        promotionVO = new Promotion_RoomQuantityVO(nameTextField.getText(),PromotionType.Hotel_Birthday, Double.valueOf(discountTextField.getText()),
+                new DateUtil(startTime.getDate()), new DateUtil(endTime.getDate()),
+                DataClass.hotelID, Integer.valueOf(roomQuantityTextField.getText()));
+        promotionBLService.addPromotion(promotionVO);
+
+    }
+
+    public void showEditView(PromotionVO promotionVO){
+        Promotion_RoomQuantityVO promotion_roomQuantityVO = (Promotion_RoomQuantityVO)promotionVO;
+        nameTextField.setText(promotionVO.promotionName);
+        discountTextField.setText(promotionVO.promotionDiscount+"");
+        roomQuantityTextField.setText(promotion_roomQuantityVO.roomQuantity+"");
+    }
+}
