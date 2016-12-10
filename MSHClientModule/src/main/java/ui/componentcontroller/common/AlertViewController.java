eca82b3b138af51d10dacac0b88600cd2d01359e
Vivian <1030518209@qq.com>
2016-12-10 20:32:22 +0800

    AlertView

---
+++ MSHClientModule/src/main/java/ui/componentcontroller/common/AlertViewController.java
@@ -0,0 +1,19 @@
+package ui.componentcontroller.common;
+
+import component.rectbutton.RectButton;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+/**
+ * Created by vivian on 16/12/10.
+ */
+public class AlertViewController {
+    @FXML
+    private Label infoLabel;
+
+    @FXML
+    private RectButton sureButton;
+
+    @FXML
+    private RectButton cancelButton;
+}
