55c9fe665ac0877cd0b576cc576fe0f8fe01d72d
KrayC <krayc425@gmail.com>
2016-12-21 15:00:27 +0800

    MD5 Encryption

--- MSHCommonModule/src/main/java/util/Encryptor.java
+++ MSHCommonModule/src/main/java/util/Encryptor.java
@@ -1,5 +1,7 @@
 package util;
 
+import java.security.MessageDigest;
+
 /**
  * Created by Kray on 2016/12/2.
  */
@@ -12,15 +14,33 @@ public class Encryptor {
      * @return 加密后的字符串
      */
     public static String encrypt(String str) {
-        boolean isTest = true;
+        boolean isTest = false;
         if(isTest){
             return str;
         }else{
+            //MD5 32位加密
             String result = "";
-            for(int i = 0; i < str.length(); ++i) {
-                result += (char)(str.charAt(i)<<1);
+            try {
+                MessageDigest md = MessageDigest.getInstance("MD5");
+                md.update(str.getBytes());
+                byte b[] = md.digest();
+                int i;
+                StringBuilder buf = new StringBuilder("");
+                for (int offset = 0; offset < b.length; offset++) {
+                    i = b[offset];
+                    if (i < 0)
+                        i += 256;
+                    if (i < 16)
+                        buf.append("0");
+                    buf.append(Integer.toHexString(i));
+                }
+                result = buf.toString();
+                System.out.println("MD5(" + str + ",32) = " + result);
+                return result;
+            }catch (Exception e){
+                e.printStackTrace();
+                return str;
             }
-            return result;
         }
     }
 
