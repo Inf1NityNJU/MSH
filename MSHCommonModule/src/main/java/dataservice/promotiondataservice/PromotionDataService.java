d111ef7a7097fa0a86add1578186723045055ed0
Vivian <1030518209@qq.com>
2017-01-01 18:04:07 +0800

    add commit

--- MSHCommonModule/src/main/java/dataservice/promotiondataservice/PromotionDataService.java
+++ MSHCommonModule/src/main/java/dataservice/promotiondataservice/PromotionDataService.java
@@ -10,17 +10,52 @@ import java.util.ArrayList;
  * Created by vivian on 16/10/13.
  */
 public interface PromotionDataService {
-    public ResultMessage addPromotion(PromotionPO promotionpo);
 
+    /**
+     * 增加一种新的营销策略
+     * @param pvo
+     * @return
+     */
+    public ResultMessage addPromotion(PromotionPO pvo);
+
+    /**
+     * 删除制定营销策略
+     * @param promotionID
+     * @return
+     */
     public ResultMessage deletePromotion(String promotionID);
 
-    public ResultMessage updatePromotion(PromotionPO newPromotionPO);
+    /**
+     * 更新某条酒店/网站促销策略的信息
+     * @param newPvo
+     * @return
+     */
+    public ResultMessage updatePromotion(PromotionPO newPvo);
 
+    /**
+     * 查找某条酒店/网站促销策略
+     * @param promotionID
+     * @return
+     */
     public PromotionPO searchByPromotionID(String promotionID);
 
+    /**
+     * 搜索某个种类的所有促销策略
+     * @param promotionType
+     * @return
+     */
     public ArrayList<PromotionPO> searchPromotionsByType(PromotionType promotionType);
 
-    public ArrayList<PromotionPO> searchHotelPromotions(String hotelID);
+    /**
+     * 查看某个酒店的所有促销策略
+     * @param HotelID
+     * @return
+     */
+    public ArrayList<PromotionPO> searchHotelPromotions(String HotelID);
 
+    /**
+     * 查看网站的所有促销策略
+     * @return
+     */
     public ArrayList<PromotionPO> searchWebPromotions();
 }
--- MSHCommonModule/src/main/java/network/promotionnetworkservice/PromotionServerNetworkService.java
+++ MSHCommonModule/src/main/java/network/promotionnetworkservice/PromotionServerNetworkService.java
@@ -12,17 +12,52 @@ import java.util.ArrayList;
  * Created by Kray on 2016/11/27.
  */
 public interface PromotionServerNetworkService extends Remote {
-    public ResultMessage addPromotion(PromotionPO promotionpo) throws RemoteException;
 
+    /**
+     * 增加一种新的营销策略
+     * @param pvo
+     * @return
+     */
+    public ResultMessage addPromotion(PromotionPO pvo) throws RemoteException;
+
+    /**
+     * 删除制定营销策略
+     * @param promotionID
+     * @return
+     */
     public ResultMessage deletePromotion(String promotionID) throws RemoteException;
 
-    public ResultMessage updatePromotion(PromotionPO newPromotionPO) throws RemoteException;
+    /**
+     * 更新某条酒店/网站促销策略的信息
+     * @param newPvo
+     * @return
+     */
+    public ResultMessage updatePromotion(PromotionPO newPvo) throws RemoteException;
 
+    /**
+     * 查找某条酒店/网站促销策略
+     * @param promotionID
+     * @return
+     */
     public PromotionPO searchByPromotionID(String promotionID) throws RemoteException;
 
+    /**
+     * 搜索某个种类的所有促销策略
+     * @param promotionType
+     * @return
+     */
     public ArrayList<PromotionPO> searchPromotionsByType(PromotionType promotionType) throws RemoteException;
 
-    public ArrayList<PromotionPO> searchHotelPromotions(String hotelID) throws RemoteException;
+    /**
+     * 查看某个酒店的所有促销策略
+     * @param HotelID
+     * @return
+     */
+    public ArrayList<PromotionPO> searchHotelPromotions(String HotelID) throws RemoteException;
 
+    /**
+     * 查看网站的所有促销策略
+     * @return
+     */
     public ArrayList<PromotionPO> searchWebPromotions() throws RemoteException;
 }
